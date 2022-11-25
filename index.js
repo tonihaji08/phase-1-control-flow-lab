ab3eab145249c45c7187870a214cf750842e1f67
Your Name <antony.adriano@stundent.moringaschool.com>
2022-11-25 21:21:09 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
-}
+  if (distance <400){
+    return "This one is on me!"
+  }else if(distance>2000 && distance<2500){
+    return "I will gladly take your thirty bucks."
+  }else if(distance >2500){
+    return "No can do."
+  }
 
-function ternaryCheckCity(){
+    
+  }
+function ternaryCheckCity(city){
   // Write your code here!
-}
+  const checkCity=(city==="NYC") ? "Ok, sounds good.": "No go."
+  return checkCity
+} 
+ternaryCheckCity("NYC")
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case "generous":
+      return "Thank you so much."
+    case 'not as generous':
+      return "Thank you."
+    default:
+      return "Bye."
+    }  
+
 }
